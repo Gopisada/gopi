e2963efc20411c8dfb1d166ba722b729877d54fc
Gopisada <93388175+Gopisada@users.noreply.github.com>
2024-04-10 22:40:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ import mysql.connector
 from flask_cors import CORS
 import json
 mysql = mysql.connector.connect(user='Gopisadasivarao', password='Gopsss@143',
-  host='74.234.2.104',
+  host='home.northeurope.cloudapp.azure.com',
   database='student')
